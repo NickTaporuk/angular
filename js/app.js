6406e41401d0113b02bf3fc500605868d14631a7
NickTaporuk <nictaporuk@yandex.ru>
2015-02-28 14:38:34 +0200

    init factory

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
 /**
  * Created by nick on 28.02.15.
  */
-angular.module('myApp',['app']);
+var app = angular.module('myApp',[]);
 
-var app = angular.module('myApp');
+app.factory('LogService',function(){
+    return {
+        log:function(arguments){
+            console.log('"'+arguments+':"',arguments);
+        }
+    }
+});
 
-app.controller('MainController',['$scope',function($scope){
-
+app.factory('AlertService',['$window',function($window){
+    return {
+        log:function(arguments){
+            $window.alert('"'+arguments+':"'+arguments);
+        }
+    }
 }]);
+
+//app.controller('MainController',['$scope','LogService','AlertService',
+app.controller('MainController',['$scope','AlertService',
+    function($scope,logger,alert){
+        $scope.name = 'Guest';
+
+        $scope.Funct = function(){
+            logger.log('Hello')
+        }
+    }
+]);
 
 
