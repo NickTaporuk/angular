0321c60090c6e81a20c02b0b2a07499b6d899596
NickTaporuk <nictaporuk@yandex.ru>
2015-02-28 14:12:39 +0200

    init directory

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nick on 28.02.15.
+ */
+angular.module('myApp',['app']);
+
+var app = angular.module('myApp');
+
+app.controller('MainController',['$scope',function($scope){
+
+}]);
+
+
